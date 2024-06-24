253bca3a6a73d77e013db0e06688baac33a58528
kgour <karangour@gmail.com>
2024-06-25 02:29:49 +0530

    corrected case sensitive repo name

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  base: "/Fairy-Folk---Movie-Website/",
   server: { host: "0.0.0.0", port: 3000 },
 });
